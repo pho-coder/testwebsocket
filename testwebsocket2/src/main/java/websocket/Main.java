e847d07e2bb7df916d4d361c70ec1590dd6b81f6
Phoenix Zhang <pho.coderzhang@gmail.com>
2017-07-05 00:48:46 +0800

    fix

--- testwebsocket2/src/main/java/websocket/Main.java
+++ testwebsocket2/src/main/java/websocket/Main.java
@@ -1,5 +1,10 @@
 package websocket;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Created by phoenix on 7/3/17.
  */
@@ -7,6 +12,19 @@ package websocket;
 
     public class Main {
         public static void main(String[] args) {
-
+            System.out.println((new Long("123")));
+            try {
+                WSClient wsClient = new WSClient("wss://be.huobi.com/ws", "market.ethcny.depth.step1", 1000L);
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            } catch (KeyManagementException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
